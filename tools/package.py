cf69ab1829ad7e3ffc7c6a0e43f6f212022885ba
Juan Medina <jamedina@gmail.com>
2015-12-19 18:53:03 +0000

    adding first scripts

---
+++ tools/package.py
@@ -0,0 +1,67 @@
+###############################################################
+## Program: package.py
+## Description: Package the Addon
+## Author: jamedina@gmail.com
+##-------------------------------------------------------------
+## Required Modules:
+###############################################################
+
+#--------------------------------------------------------------
+# ACTUAL CODE BELLOW, DO NOT EDIT
+#--------------------------------------------------------------
+
+#system imports
+import logging
+import os
+import sys
+from os import walk
+import codecs
+import re
+import zipfile
+
+from utils import AddonInfo
+
+# Log object
+log = None
+
+def CreateZip(zip_path, addon, license_file):
+
+  log.info("Creating zip file : '%s'", zip_path)
+
+  zip_object = zipfile.ZipFile(zip_path, 'w',zipfile.ZIP_DEFLATED)
+
+  for file_original in addon.files:
+    file_partial = file_original.replace(addon.src_folder,"")
+    log.info("Adding file to zip : '%s'", file_partial)
+    zip_object.write(file_original,file_partial)
+
+  for folder in addon.main_folders:
+    folder_partial = folder.replace(addon.src_folder,"")
+    lincese_path = folder_partial+os.sep+"LICENSE"
+    log.info("Adding file to zip : '%s'", lincese_path)
+    zip_object.write(license_file,lincese_path)
+
+  zip_object.close()
+
+  log.info("Zip file created: '%s'", zip_path)
+
+if __name__ == '__main__':
+
+  LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+  logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+  log = logging.getLogger(__name__)
+
+  try:
+    addon = AddonInfo()
+
+    zip_path = os.path.abspath(os.path.join(os.pardir, "dist"))+os.sep+addon.name+"_v"+addon.version+".zip"
+
+    license_file = os.path.abspath(os.pardir)+os.sep+"LICENSE"
+
+    CreateZip(zip_path, addon, license_file)
+
+
+  except Exception as ex:
+    logging.error(ex, exc_info = True)
+    raise ex
---
+++ tools/utils.py
@@ -0,0 +1,69 @@
+import os
+import logging
+import re
+import codecs
+from os import walk
+
+class AddonInfo:
+
+  def __init__(self):
+
+    self.log = logging.getLogger(self.__class__.__name__)
+
+    self.name = self.GetAddonName()
+
+    self.src_folder = os.path.abspath(os.path.join(os.pardir, "src"))
+
+    self.toc = self.src_folder + os.sep + self.name + os.sep + self.name + ".toc"
+
+    self.version = self.GetAddonVersion()
+
+    self.folders, self.files, self.main_folders = self.GetSources()
+
+  def GetAddonName(self):
+    addon_folder = os.path.abspath(os.pardir)
+
+    parent_folder = os.path.abspath(os.path.join(addon_folder, os.pardir))
+
+    addon_name = addon_folder.replace(parent_folder+os.sep,"")
+
+    self.log .info("Addon is : '%s'", addon_name)
+
+    return addon_name
+
+  def GetAddonVersion(self):
+
+    version = None
+    reg_exp = re.compile(ur'## Version..(.*)')
+
+    with codecs.open(self.toc, encoding='utf-8') as input_file:
+      for line in input_file.readlines():
+        match = re.search(reg_exp, line)
+        if not(match is None):
+          version = match.group(1)
+          version = version.replace("\r","")
+          break
+
+    self.log .info("Addon version is : '%s'", version)
+
+    return version
+
+  def GetSources(self):
+
+    self.log.info("reading src folder [%s]", self.src_folder)
+
+    folders = []
+    files = []
+    main_folders = []
+
+    for (dirpath, dirnames, filenames) in walk(self.src_folder):
+        folders.append(dirpath)
+        for filename in filenames:
+          files.append(dirpath + os.sep + filename)
+
+    for folder in folders:
+      folder_partial = folder.replace(self.src_folder+os.sep,"")
+      if(re.search(ur"\\",folder_partial) is None):
+        main_folders.append(folder)
+
+    return folders,files,main_folders
